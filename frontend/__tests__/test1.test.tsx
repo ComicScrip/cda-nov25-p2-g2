5866d897996e2e98f2bbbcaddc2315dffabe4bfa
Omaya404 <omaya.battour@gmail.com>
2026-02-04 14:52:10 +0100

    init test1

---
+++ frontend/__tests__/test1.test.tsx
@@ -0,0 +1,21 @@
+import { MockedProvider } from "@apollo/client/testing/react";
+import { render, screen } from "@testing-library/react";
+import Header from "@/components/Header";
+
+// Mock next/router
+jest.mock("next/router", () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+  }),
+}));
+
+describe("Heading", () => {
+  it("composed h1", () => {
+    render(
+      <MockedProvider>
+        <Header />
+      </MockedProvider>,
+    );
+    expect(screen.getByRole("heading", { level: 1 })).toBeInTheDocument();
+  });
+});
