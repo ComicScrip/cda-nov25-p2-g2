44936c58a38f0aa9b31d2892379e36bea8e67688
Evan Cattarossi <abracadabraunnoire@gmail.com>
2026-02-13 10:40:44 +0100

    intégration test and deploy

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "@/components/Layout";
 export default function Home() {
   return (
     <Layout pageTitle="Accueil">
-      <h2 className="text-xl mb-4 p-4">Welcome !</h2>
+      <h2 className="text-xl mb-4 p-4">Welcome to BabyBoard!</h2>
     </Layout>
   );
 }
