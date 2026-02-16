64d5f55ac054321a386d5a06d013b8f006d8cd88
Denis <dpatillot@gmail.com>
2026-02-16 11:35:51 +0100

    login page + responsive

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,22 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <Layout pageTitle="Accueil">
-      <h2 className="text-xl mb-4 p-4">Welcome !</h2>
+      <img src="/babyboardlogo.png" className="md:w-[40%] md:m-auto md:max-w-[600px]"/>
+      <div className="p-4 max-w-[400px] mx-auto mt-15 md:max-w-[600px] md:mt-0">
+      <form className="flex flex-col items-center text-[#1b3c79] md:relative md:bottom-10">
+        <input type="email" placeholder="Email" className="text-xl text-center bg-[#d4efff] rounded-4xl px-2 py-3 w-[75%] mb-2 md:text-4xl md:rounded-[50] md:py-6 md:mb-6" />
+        <input type="password" placeholder="Mot de passe" className="text-xl text-center bg-[#d4efff] rounded-4xl px-2 py-3 w-[75%] mb-0 md:text-4xl md:rounded-[50] md:py-6" />
+        <Link href="/" className="hover:underline">
+          <p className="text-[16px] md:text-2xl">mot de passe oublié ?</p>
+        </Link>
+        <input type="button" value="Se connecter" className="text-xl text-center bg-[#d4efff] rounded-4xl px-2 py-3 w-[65%] mt-18 mb-2 border-2 border-transparent hover:cursor-pointer hover:border-2 hover:border-[#88D3FF] md:text-4xl md:rounded-[50] md:py-5 md:w-[75%]" />
+        <p className="text-[16px] md:text-2xl">Besoin d'aide ? <Link href="/" className="hover:underline">Contact crèche</Link></p>
+      </form>
+      </div>
     </Layout>
+    
   );
 }
