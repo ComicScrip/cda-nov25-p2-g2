b15bd313aa2bde0e11b9258509297c0faecf1ede
Denis <dpatillot@gmail.com>
2026-02-19 11:25:49 +0100

    update Footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,9 +5,9 @@ export default function Footer() {
     const router = useRouter();
     return( 
       <footer className="absolute bottom-0 flex w-full mx-auto gap-1 justify-center">
-        <img src="home.png" alt="" className="w-25 h-20" />
-        <img src="calendrier.png" alt="" className="w-30 h-25" />
-        <img src="chat.png" alt="" className="w-25 h-20" />
+        <img src="home.png" alt="" className="w-30 h-25" />
+        <img src="calendrier.png" alt="" className="w-40 h-30" />
+        <img src="chat.png" alt="" className="w-30 h-25" />
       </footer>
     );
 }  
